56216f6bb5a73dfcdaabc598ccde2a0c9ddffc76
Jonathan Cruz <cruzjonathan.e@gmail.com>
2022-08-24 19:49:18 -0300

    Clase 11 - Eventos del teclado 2

--- Clase_10/script.js
+++ Clase_10/script.js
@@ -1,12 +1,31 @@
+const texto_1 = document.querySelector('#texto-1');
+const texto_2 = document.querySelector('#texto-2');
+const contenedor =  document.querySelector('body');
 
+
+contenedor.addEventListener('keydown', function(evento){
+    
+    console.log('evento keydown ', evento.key)
+    texto_1.textContent = `Evento del teclado con keydown ${evento.key}`;
+})
+
+contenedor.addEventListener('keypress', function(evento){
+    
+    console.log('evento keypress ', evento.key);
+    texto_2.textContent = evento.key;
+})
+
+
+/*
 window.addEventListener('keydown', function(evento){
     evento.preventDefault();
     console.log('evento keydown ', evento.key)
+    texto_1.textContent = `Evento del teclado con keydown ${evento.key}`;
 })
 
 window.addEventListener('keypress', function(evento){
-    window.addEventListener('keydown', function(evento){
-        console.log('evento keydown ', evento.key)
-    })
-    console.log('evento keypress ', evento.key)
+    evento.preventDefault();
+    console.log('evento keypress ', evento.key);
+    texto_2.textContent = evento.key;
 })
+*/
