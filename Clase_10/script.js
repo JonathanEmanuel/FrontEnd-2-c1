12820de350c0394f9262332f6f58c0f21ec1d0e3
Jonathan Cruz <cruzjonathan.e@gmail.com>
2022-08-24 21:19:11 -0300

    Clase 11 - Formularios

--- Clase_10/script.js
+++ Clase_10/script.js
@@ -4,12 +4,13 @@ const contenedor =  document.querySelector('body');
 
 
 contenedor.addEventListener('keydown', function(evento){
-    
+    evento.preventDefault();
     console.log('evento keydown ', evento.key)
     texto_1.textContent = `Evento del teclado con keydown ${evento.key}`;
 })
 
 contenedor.addEventListener('keypress', function(evento){
+    evento.preventDefault();
     
     console.log('evento keypress ', evento.key);
     texto_2.textContent = evento.key;
